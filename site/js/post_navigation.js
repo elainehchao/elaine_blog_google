778cacae90521a9b9a3f45f3295344871bfb17f7
e5chao <e5chao@ucsd.edu>
2019-12-31 20:11:48 -0800

    adding jquery library, adding rudimentary post navigation functionality

---
+++ site/js/post_navigation.js
@@ -0,0 +1,51 @@
+const MIN_POSTS = 1;
+const NUM_POSTS = 2;
+
+var currentPost = NUM_POSTS;
+
+function getPostName(num) {
+    return "post" + num + ".html";
+}
+
+function loadPost(num) {
+    console.log(getPostName(num));
+    $("#blog_content").load(getPostName(num));
+    currentPost = num;
+    validatePrevNextButtons();
+};
+
+function loadMain() {
+    loadPost(NUM_POSTS);
+}
+
+function loadCurrent() {
+    loadPost(currentPost);
+}
+
+function loadPrevious() {
+    if (currentPost > 1) {
+        currentPost = currentPost - 1;
+        loadPost(currentPost);
+    }
+}
+
+function loadNext() {
+    if (currentPost < NUM_POSTS) {
+        currentPost = currentPost + 1;
+        loadPost(currentPost);
+    }
+}
+
+function validatePrevNextButtons() {
+    if (currentPost < NUM_POSTS) {
+        $("#next_post_button").show();
+    } else {
+        $("#next_post_button").hide();
+    }
+
+    if (currentPost > MIN_POSTS) {
+        $("#previous_post_button").show();
+    } else {
+        $("#previous_post_button").hide();
+    }
+}
