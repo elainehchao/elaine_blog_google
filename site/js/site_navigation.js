1948e7b76fc68b1e83fac4092a853645fc00d59a
e5chao <e5chao@ucsd.edu>
2020-01-01 17:46:06 -0800

    adding post content

---
+++ site/js/site_navigation.js
@@ -0,0 +1,10 @@
+function loadHeading() {
+    $("#header").load("components.html #header_component");
+}
+
+function loadSideNav(selected_page) {
+    $("#right_container").load("components.html #side_nav", function () {
+        $("#" + selected_page + "_nav").addClass("selected");
+    });
+
+}
